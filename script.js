2584789e5609703968bb334bef19b155d70a12a6
ItsVasconcellos <fernando.asv@uol.com.br>
2023-10-05 18:25:49 -0300

    feat: Link do linkedin dos membros

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function toggleMode(){
     body.classList.toggle('light');
     const img = document.querySelector("#profile img");
     if(body.classList.contains('light')){
-        img.setAttribute('src', './assets/avatar-light.png');
+        img.setAttribute('src', './assets/avatar.jpg');
     }
     else{
-        img.setAttribute('src', './assets/Avatar.png')
+        img.setAttribute('src', './assets/Avatar.jpg')
     }
 }
